d0b5e6898b701dc1ee7bb9cc998496c34fa57f15
jultty <juno.t@tuta.io>
2023-09-19 16:38:18 -0300

    Basic argument parser and logger implementations

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 use std::env;
 
-const DEBUG: bool = true;
+struct LoggerOptions {
+    debug: bool,
+}
 
-fn logger(message: &str) {
-    if DEBUG {
+fn logger(message: &str, opts: &LoggerOptions) {
+    if opts.debug {
         println!("{message}");
     }
 }
 
-fn parse_arguments() {
-    logger("Reading arguments");
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+struct Arguments {
+    debug: bool,
+}
+
+fn parse_arguments() -> Arguments {
+    let arguments: Vec<String> = env::args().collect();
+    let mut log_opts = LoggerOptions { debug: false };
+
+    let mut return_arguments: Arguments = Arguments { debug: false };
+
+    for arg in &arguments {
+        if arg == "--debug" {
+            return_arguments.debug = true;
+            log_opts.debug = true;
+        }
+        let mut log_message = "Read argument ".to_owned();
+        log_message.push_str(arg);
+        logger(&log_message, &log_opts);
+    }
+
+    logger("Done reading arguments", &log_opts);
+
+    return return_arguments;
 }
 
 fn main() {
-    parse_arguments();
+    let _arguments = parse_arguments();
 }
