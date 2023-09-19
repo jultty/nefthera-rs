8f5414a2d5ab7d23e6975db0031adfceaf6af3af
jultty <juno.t@tuta.io>
2023-09-19 19:07:37 -0300

    Main loop parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::io;
 
 struct Arguments {
     debug: bool,
+    start: bool,
 }
 
 struct LoggerOptions {
@@ -17,12 +19,18 @@ fn logger(message: &str, opts: &LoggerOptions) {
 fn parse_arguments(log_opts: &mut LoggerOptions) -> Arguments {
     let arguments: Vec<String> = env::args().collect();
 
-    let mut return_arguments: Arguments = Arguments { debug: false };
+    let mut return_arguments: Arguments = Arguments {
+        debug: false,
+        start: false,
+    };
 
     for arg in &arguments {
         if arg == "--debug" {
             return_arguments.debug = true;
             log_opts.debug = true;
+        }
+        if arg == "--start" {
+            return_arguments.start = true;
         }
 
         let mut log_message = "Read argument ".to_owned();
@@ -35,9 +43,37 @@ fn parse_arguments(log_opts: &mut LoggerOptions) -> Arguments {
     return_arguments
 }
 
-fn main() {
-    let mut log_opts = LoggerOptions { debug: false };
-    let _arguments = parse_arguments(&mut log_opts);
+fn parse_input(input: String) {
+    let log_opts = LoggerOptions { debug: false };
 
-    logger("", &log_opts);
+    match input.as_str() {
+        "move" => logger("Action triggered: move", &log_opts),
+        _other => logger("Action triggered: default", &log_opts),
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut log_opts = LoggerOptions { debug: false };
+    let arguments = parse_arguments(&mut log_opts);
+
+    if arguments.start {
+        loop {
+            logger("Starting main loop per arguments", &log_opts);
+
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    if input == "exit" {
+                        break;
+                    } else {
+                        parse_input(input);
+                    }
+                }
+                Err(error) => println!("Error: {error}"),
+            }
+        }
+    }
+
+    logger("Exiting: End of file", &log_opts);
+    Ok(())
 }
